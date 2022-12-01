e8e838e0e744f3ca7e352ffa6a41537f35e3bafb
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2022-12-01 11:35:13 +0530

    Create main.cpp

---
+++ templates/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define vt vector
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin()
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define F first
+#define S second
+#define mp make_pair
+
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+
+template<typename T> void read(T &x) {
+    cin >> x;
+}
