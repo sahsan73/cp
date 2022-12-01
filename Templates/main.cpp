4b23549d9527595e95083e816f4ef70936706064
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2022-12-01 11:13:12 +0530

    Update main.cpp

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -1,65 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mod = 1e9 + 7;
-const double pi = acos(-1);
-const char nl = '\n';
-const double eps = 1e-9;
-
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> pii;
-
-#define F first
-#define S second
 
 #define vt vector
-#define pq priority_queue
-#define mp make_pair
 #define pb push_back
-
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 
-#define rep(i, k, n) for(int i = k; i < n; ++i)
-#define repe(a, x) for(auto &a: x)
-#define repr(i, n, k) for(int i = n; i >= k; --i)
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+#define F first
+#define S second
+#define mp make_pair
 
-template<class T> void read(T &x) {
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+
+template<typename T> void read(T &x) {
 	cin >> x;
 }
 void read(double &d) {
-	string s;
-	read(s);
+	string s; read(s);
 	d = stod(s);
 }
-void read(long double &d) {
-	string s;
-	read(s);
+void read(ld &d) {
+	string s; read(s);
 	d = stold(s);
 }
-template<class H, class... T> void read(H &h, T&... t) {
-	read(h);
-	read(t...);
+template<typename T, typename...H> void read(T &t, H&... h) {
+	read(t);
+	read(h...);
 }
-template<class T> void read(vt<T> &x) {
-	repe(a, x) read(a);
+template<typename T> void read(vt<T> &v) {
+	for(auto &x: v) read(x);
 }
+
+
 
 void solve() {
 	;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
 	int t = 1;
-	// read(t);
-	while (t--) solve();
+	cin >> t;
+	while(t--) solve();
 	return 0;
 }
