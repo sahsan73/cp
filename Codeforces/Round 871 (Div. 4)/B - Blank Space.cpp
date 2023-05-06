1bc3b35695e43071b7690dcd02a0e851f715864c
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-05-06 22:33:51 +0530

    Create B - Blank Space.cpp

---
+++ Codeforces/Round 871 (Div. 4)/B - Blank Space.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+	int n; cin >> n;
+	vector<int> arr(n);
+	for(int i=0; i<n; ++i) cin >> arr[i];
+ 
+	int mx=0, cnt=0;
+	for(int i=0; i<n; ++i) {
+		if(arr[i]) {
+			mx=max(mx, cnt);
+			cnt=0;
+		} else {
+			++cnt;
+		}
+	}
+	cout << max(mx, cnt) << '\n';
+}
+ 
+ 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+ 
+	int t=1;
+	cin >> t;
+	while(t--) solve();
+	return 0;
+}
