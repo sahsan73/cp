5508241edca9a62e3b6bdc1c73541865001f36bf
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-08-15 16:05:46 +0530

    Update Message Route.cpp

--- Problems-&&-Solutions/CSES/Graph Algorithms/Message Route.cpp
+++ Problems-&&-Solutions/CSES/Graph Algorithms/Message Route.cpp
@@ -3,11 +3,11 @@ Problem Statement: https://cses.fi/problemset/task/1667/
 */
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 const int mxN=1e5;
-int n, m, p[mxN], vis[mxN];
+int n, m, p[mxN];
 vector<int> adj[mxN];
-
+ 
 int main() {
 	cin >> n >> m;
 	for(int i=0, a, b; i<m; ++i) {
@@ -16,36 +16,32 @@ int main() {
 		adj[b].push_back(a);
 	}
 	
-	iota(p, p+n, 0);
-	queue<int> q;
-	q.push(0);
-	vis[0]=1;
-	while(!q.empty()) {
-		int u=q.front();
-		q.pop();
+	memset(p, -1, sizeof(p));
+	queue<int> qu;
+	qu.push(0);
+	p[0]=0;
+	while(!qu.empty()) {
+		int u=qu.front();
+		qu.pop();
 		for(int v: adj[u]) {
-			if(!vis[v]) {
-				q.push(v);
+			if(p[v]<0) {
+				qu.push(v);
 				p[v]=u;
-				vis[v]=1;
 			}
 		}
 	}
-	
-	vector<int> a;
-	int i=n-1;
-	for(; i^p[i]; i=p[i])
-		a.push_back(i+1);
-	;
-	if(i) {
+	if(p[n-1]<0)
 		cout << "IMPOSSIBLE";
-	} else {
-		a.push_back(1);
+	else {
+		vector<int> a;
+		for(int i=n-1; i^p[i]; i=p[i])
+			a.push_back(i);
+		a.push_back(0);
 		reverse(a.begin(), a.end());
 		cout << a.size() << "\n";
-		for(int j=0; j<a.size(); ++j)
-			cout << a[j] << " ";
+		for(int i=0; i<a.size(); ++i)
+			cout << a[i]+1 << " ";
 	}
-
+ 
 	return 0;
 }
