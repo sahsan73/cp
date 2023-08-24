74ff02a6a6b4690d3f685498eb50c4b70f0cf3ec
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-08-24 19:41:39 +0530

    Update 721. Accounts Merge.cpp

--- Problems-&&-Solutions/LeetCode/Graph/721. Accounts Merge.cpp
+++ Problems-&&-Solutions/LeetCode/Graph/721. Accounts Merge.cpp
@@ -1,63 +1,60 @@
 /*
-Problem Statement: 
+Problem Statement: https://leetcode.com/problems/accounts-merge/description/
 */
-struct DisjointSetUnion {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+APPROACH: DSU
+*/
+class Solution {
+private:
     int p[1000], s[1000];
-    DisjointSetUnion(int n) {
-        iota(p, p+n, 0);
-        fill(s, s+n, 1);
-    }
 
     int find(int u) {
-        return u^p[u] ? p[u] = find(p[u]) : u;
+        return u^p[u] ? p[u]=find(p[u]) : u;
     }
 
     void join(int u, int v) {
         if((u=find(u)) == (v=find(v)))
             return;
-        
-        if(s[u] < s[v])
-            swap(u, v);
+        if(s[u] < s[v]) swap(u, v);
         p[v] = u;
         s[u] += s[v];
     }
-};
 
-class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        // each account represents a node (i.e., accounts[i] <-- node)
-        // now let's assign each mail with the node
         int n = (int)accounts.size();
+        iota(p, p+n, 0);
+        fill(s, s+n, 1);
+
+        // each account represents a node (i.e., accounts[i] --> a node with id i)
+        // now, let's assign each email with its node id
         unordered_map<string, int> mp;
-        DisjointSetUnion dsu(n);
         for(int i=0; i<n; ++i) {
             for(int j=1; j<accounts[i].size(); ++j) {
-                string s = accounts[i][j];
-                // if a node id is already assigned to a mail, means the accounts ith and already
-                // assigned value belong to the same person
-                if(mp.count(s)) {
-                    dsu.join(i, mp[s]);
+                string email = accounts[i][j];
+                if(!mp.count(email)) {
+                    mp[email] = i;
                 } else {
-                    mp[s] = i;
+                    // if an email already assigned to a node id, this means curr ith account (node i)
+                    // and already assigned account (id --> mp[email]) belong to the same user.
+                    join(mp[email], i);
                 }
             }
         }
 
-        // let's merge the mails for same accounts
         unordered_map<int, set<string>> mp2;
-        for(auto &p: mp) {
-            int i = dsu.find(p.second);
-            mp2[i].insert(p.first);
+        for(auto &[email, i]: mp) {
+            int id = find(i);
+            mp2[id].insert(email);
         }
-
-        // final list of accounts
+        
         vector<vector<string>> answer;
-        for(auto &p: mp2) {
-            string s = accounts[p.first][0];
-            answer.push_back({s});
-            for(auto &s2: p.second)
-                answer.back().push_back(s2);
+        for(auto &[id, st]: mp2) {
+            vector<string> finalAccount(st.begin(), st.end());
+            finalAccount.insert(finalAccount.begin(), accounts[id][0]);
+            answer.push_back(finalAccount);
         }
         return answer;
     }
