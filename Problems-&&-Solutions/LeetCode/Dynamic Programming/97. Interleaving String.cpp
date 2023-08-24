e7ed8938a4edb7e21e638bd8164dbbaa95cfda2b
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-08-24 10:24:22 +0530

    Update 97. Interleaving String.cpp

--- Problems-&&-Solutions/LeetCode/Dynamic Programming/97. Interleaving String.cpp
+++ Problems-&&-Solutions/LeetCode/Dynamic Programming/97. Interleaving String.cpp
@@ -3,6 +3,36 @@ Problem Statement: https://leetcode.com/problems/interleaving-string/description
 
 extn. of LCS problem
 */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Implementation: Bottom-Up
+*/
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = (int)s1.size(), n = (int)s2.size();
+        if(m+n != s3.size()) return 0;
+
+        int dp[101][101];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1, dp[1][0] = s1.front()==s3.front(), dp[0][1] = s2.front()==s3.front();
+        for(int i=0; i<=m; ++i) {
+            for(int j=0; j<=n; ++j) {
+                if(i>=1 && s1[i-1] == s3[(i+j-1)] && dp[i-1][j])
+                    dp[i][j] = 1;
+                if(j>=1 && s2[j-1] == s3[(i+j-1)] && dp[i][j-1])
+                    dp[i][j] = 1;
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Implementation: Top-Down
+*/
 class Solution {
 private:
     string s1, s2, s3;
