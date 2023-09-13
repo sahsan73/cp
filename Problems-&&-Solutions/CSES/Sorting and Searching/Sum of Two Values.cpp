ecfef4032403324ed4be4008b1713a3f565becb0
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-09-13 13:34:34 +0530

    Update Sum of Two Values.cpp

--- Problems-&&-Solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
+++ Problems-&&-Solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -1,6 +1,42 @@
 /*
 Problem Statement: https://cses.fi/problemset/task/1640
 */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+#define ar array
+
+const int mxN=2e5;
+int n;
+ll x;
+ar<ll, 2> a[mxN];
+
+int main() {
+	cin >> n >> x;
+	for(int i=0; i<n; ++i)
+		cin >> a[i][0], a[i][1]=i+1;
+
+	/*
+	 * If you try to use unordered_map, you will get TLE. As because, in worst case, count
+  	 * operation works in O(n) time.
+	*/
+	map<ll, ll> mp;
+	for(int i=0; i<n; ++i) {
+		if(mp.count(x-a[i][0])) {
+			cout << mp[x-a[i][0]] << " " << a[i][1];
+			return 0;
+		}
+		mp[a[i][0]]=a[i][1];
+	}
+	cout << "IMPOSSIBLE";
+
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
