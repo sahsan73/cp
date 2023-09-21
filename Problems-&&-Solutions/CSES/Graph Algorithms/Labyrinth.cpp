ac4b4b62eea6ff0cc4244d3fa5b570638504dd5e
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-09-21 09:07:47 +0530

    Update

--- Problems-&&-Solutions/CSES/Graph Algorithms/Labyrinth.cpp
+++ Problems-&&-Solutions/CSES/Graph Algorithms/Labyrinth.cpp
@@ -4,67 +4,78 @@ Problem Statement: https://cses.fi/problemset/task/1193
 
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 #define ar array
 
-const int mxN=1e3, d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
-const char dir[4]={'U', 'R', 'D', 'L'};
-int n, m, si, sj, ti, tj, d[mxN][mxN];
-string s[mxN], p[mxN];
+const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
+int n, m, p[1000][1000];
+string s[1000], dir="URDL";
+ar<int, 2> a, b;
 
-bool e(int i, int j) {
-	return (i>=0&&i<n&&j>=0&&j<m&&s[i][j]=='.');
-}
-
-int main() {
+void solve() {
 	cin >> n >> m;
 	for(int i=0; i<n; ++i) {
 		cin >> s[i];
 		for(int j=0; j<m; ++j) {
 			if(s[i][j]=='A')
-				si=i, sj=j, s[i][j]='#';
+				a={i, j}, s[i][j]='#';
 			if(s[i][j]=='B')
-				ti=i, tj=j, s[i][j]='.';
+				b={i, j}, s[i][j]='.';
 		}
-		p[i]=string(m, 0);
 	}
 
-	queue<ar<int, 2>> qu;
-	qu.push({si, sj});
-	while(!qu.empty()) {
-		ar<int, 2> u=qu.front();
-		qu.pop();
+	auto e=[&](int i, int j) {
+		return (i>=0&&i<n&&j>=0&&j<m&&s[i][j]=='.');
+	};
+	
+	memset(p, -1, sizeof(p));
+	queue<ar<int, 2>> q;
+	q.push(a);
+	while(!q.empty()) {
+		ar<int, 2> u=q.front();
+		q.pop();
+		if(u==b)
+			break;
 		for(int k=0; k<4; ++k) {
 			int ni=u[0]+d4i[k];
 			int nj=u[1]+d4j[k];
 			if(e(ni, nj)) {
-				qu.push({ni, nj});
-				d[ni][nj]=k;
-				p[ni][nj]=dir[k];
+				q.push({ni, nj});
 				s[ni][nj]='#';
+				p[ni][nj]=k;
 			}
 		}
 	}
-
-	if(p[ti][tj]) {
-		cout << "YES" << "\n";
-		string t;
-		while(ti^si||tj^sj) {
-			t+=p[ti][tj];
-			int k=d[ti][tj];
-			ti=ti-d4i[k];
-			tj=tj-d4j[k];
-			/*
-			 * Another way to get the parent
-			 * int k=p[ti][tj]^2
-			 * ti+=d4i[k]
-			 * tj+=d4j[k]
-			 */
-		}
-		reverse(t.begin(), t.end());
-		cout << t.size() << "\n" << t;
-	} else
+	if(p[b[0]][b[1]]==-1) {
 		cout << "NO";
+		return;
+	}
+	string res;
+	while(b[0]^a[0]||b[1]^a[1]) {
+		int k=p[b[0]][b[1]];
+		res.push_back(dir[k]);
+		b[0]-=d4i[k];
+		b[1]-=d4j[k];
+		/*
+		 * Another way to get the parent
+		 * k^=2;  <-- This will 1 to 3 and 0 to 2
+   		 * so it is same if we add upward direction instead of subtract downward direction
+		 * b[0]+=d4i[k]
+		 * b[1]+=d4j[k]
+		 */
+	}
+	reverse(res.begin(), res.end());
+	cout << "YES\n" << res.size() << "\n" << res;
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
