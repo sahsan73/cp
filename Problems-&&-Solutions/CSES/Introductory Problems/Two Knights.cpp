c7b9c023a88b19fbb31d9cceb2f14c3bdb6ed1bb
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-10-08 21:20:20 +0530

    Update Two Knights.cpp

--- Problems-&&-Solutions/CSES/Introductory Problems/Two Knights.cpp
+++ Problems-&&-Solutions/CSES/Introductory Problems/Two Knights.cpp
@@ -1,5 +1,7 @@
 /*
 Problem Statement: https://cses.fi/problemset/task/1072
+
+https://math.stackexchange.com/questions/3274520/two-knight-on-chessboard/3274545#3274545?newreg=7f69b8ac0ee9439a98094f324b7d9d21
 */
 
 #include <bits/stdc++.h>
