a375406a6c11d4466aebf99600322e5faa2a615a
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-10-12 23:26:51 +0530

    Update Apartments.cpp

--- Problems-&&-Solutions/CSES/Sorting and Searching/Apartments.cpp
+++ Problems-&&-Solutions/CSES/Sorting and Searching/Apartments.cpp
@@ -1,6 +1,7 @@
 /*
 Problem Statement: https://cses.fi/problemset/task/1084
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
@@ -8,30 +9,35 @@ using ll=long long;
 #define ar array
 
 const int mxN=2e5;
-int n, m, k;
-ll a[mxN], b[mxN];
+int n, m, k, a[mxN], b[mxN];
 
-int main() {
+void solve() {
 	cin >> n >> m >> k;
 	for(int i=0; i<n; ++i)
 		cin >> a[i];
 	for(int i=0; i<m; ++i)
 		cin >> b[i];
-
 	sort(a, a+n);
 	sort(b, b+m);
-	int cnt=0, i=0, j=0;
-	while(i<n&&j<m) {
-		if(b[j]<a[i]-k) {
-			++j;
-		} else if(a[i]-k<=b[j]&&b[j]<=a[i]+k) {
-			++cnt;
-			++i, ++j;
-		} else {
+	int ans=0;
+	for(int i=0, j=0; i<n&&j<m;) {
+		if(a[i]-k<=b[j]&&b[j]<=a[i]+k)
+			++ans, ++i, ++j;
+		else if(a[i]<b[j])
 			++i;
-		}
+		else
+			++j;
 	}
-	cout << cnt;
+	cout << ans;
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
