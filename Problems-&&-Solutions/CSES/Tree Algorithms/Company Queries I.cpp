b0d672f4ff2581e988c62c0b201a635ebd877ca9
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-10-25 10:30:25 +0530

    Update Company Queries I.cpp

--- Problems-&&-Solutions/CSES/Tree Algorithms/Company Queries I.cpp
+++ Problems-&&-Solutions/CSES/Tree Algorithms/Company Queries I.cpp
@@ -1,13 +1,56 @@
+/*
+Problem Statement: https://cses.fi/problemset/task/1687/
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
-#define vt vector
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
+#define ar array
 
 const int mxN=2e5;
-int n, q, d[mxN], anc[mxN][20];
+int n, q, e[mxN], anc[mxN][20];
+
+void solve() {
+	cin >> n >> q;
+	memset(anc, -1, sizeof(anc));
+	for(int i=1; i<n; ++i) {
+		cin >> e[i], --e[i];
+		anc[i][0]=e[i];
+	}
+	for(int j=1; j<20; ++j)
+		for(int i=1; i<n; ++i)
+			anc[i][j]=~anc[i][j-1]?anc[anc[i][j-1]][j-1]:-1;
+	while(q--) {
+		int x, k;
+		cin >> x >> k, --x;
+		for(int j=19; ~j; --j)
+			if((1<<j)&k)
+				x=~x?anc[x][j]:-1;
+		cout << (~x?x+1:x) << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+#define ar array
+
+const int mxN=2e5;
+int n, q, d[mxN], e[mxN], anc[mxN][20];
 vector<int> adj[mxN];
 
 void dfs(int u=0, int p=-1) {
@@ -22,30 +65,34 @@ void dfs(int u=0, int p=-1) {
 	}
 }
 
+void solve() {
+	cin >> n >> q;
+	for(int i=1; i<n; ++i) {
+		cin >> e[i], --e[i];
+		adj[e[i]].push_back(i);
+	}
+	dfs();
+	while(q--) {
+		int x, k;
+		cin >> x >> k, --x;
+		if(k>d[x]) {
+			cout << "-1\n";
+		} else {
+			for(int j=19; ~j; --j)
+				if((1<<j)&k)
+					x=anc[x][j];
+			cout << x+1 << "\n";
+		}
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> q;
-	for(int i=1, p; i<n; ++i) {
-		cin >> p, --p;
-		adj[p].pb(i);
-	}
-
-	dfs();
-	while(q--) {
-		int x, k;
-		cin >> x >> k, --x; 
-		if(d[x]<k) {
-			cout << -1 << "\n";
-		} else {
-			for(int j=19; ~j; --j) {
-				if(k&(1<<j))
-					x=anc[x][j];
-			}
-			cout << x+1 << "\n";
-		}
-	}
-
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
