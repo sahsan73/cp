a36a6eeb344a542c189b5963cd3b2e74342c1c58
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-10-27 18:23:45 +0530

    Create Planets Queries II.cpp

---
+++ Problems-&&-Solutions/CSES/Graph Algorithms/Planets Queries II.cpp
@@ -0,0 +1,87 @@
+/*
+Problem Statatement: https://cses.fi/problemset/task/1160/
+
+Resources: https://usaco.guide/problems/cses-1160-planets-queries-ii/solution
+
+Credit: William Lin
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+#define ar array
+
+const int mxN=2e5;
+// cr-->cycle root, cs-->cycle size
+int n, q, t[mxN], vis[mxN], d[mxN], rt[mxN], cr[mxN], rp[mxN], dt, ds[mxN], de[mxN], cs[mxN];
+vector<int> adj[mxN], cyc;
+
+void dfs(int u) {
+	ds[u]=dt++;
+	for(int v: adj[u]) {
+		if(rt[v]^v) {
+			d[v]=d[u]+1;
+			rt[v]=rt[u];
+			cr[v]=cr[u];
+			dfs(v);
+		}
+	}
+	de[u]=dt;
+}
+
+void solve() {
+	cin >> n >> q;
+	for(int i=0; i<n; ++i) {
+		cin >> t[i], --t[i];
+		adj[t[i]].push_back(i);
+	}
+	memset(rt, -1, 4*n);
+	for(int i=0; i<n; ++i) {
+		if(~rt[i])
+			continue;
+		int u=i;
+		while(!vis[u]) {
+			vis[u]=1;
+			u=t[u];
+		}
+		while(!cyc.size()||u^cyc[0]) {
+			rp[u]=cyc.size();
+			cyc.push_back(u);
+			rt[u]=u;
+			cr[u]=i;
+			u=t[u];
+		}
+		for(int ci: cyc)
+			dt=0, dfs(ci), cs[ci]=cyc.size();
+		cyc.clear();
+	}
+	while(q--) {
+		int a, b;
+		cin >> a >> b, --a, --b;
+		int ans=-1;
+		if(cr[a]==cr[b]) {
+			if(rt[b]==b) {
+				ans=d[a];
+				a=rt[a];
+				ans+=(rp[b]-rp[a]+cs[a])%cs[a];
+			} else {
+				if(rt[a]==rt[b]&&ds[b]<=ds[a]&&ds[a]<de[b]) {
+					ans=d[a]-d[b];
+				}
+			}
+		}
+		cout << ans << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
