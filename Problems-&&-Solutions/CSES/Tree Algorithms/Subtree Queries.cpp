cab8bf6a90ae7190f36bdf70b3b29de44eb918fc
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-10-27 10:57:58 +0530

    Update Subtree Queries.cpp

--- Problems-&&-Solutions/CSES/Tree Algorithms/Subtree Queries.cpp
+++ Problems-&&-Solutions/CSES/Tree Algorithms/Subtree Queries.cpp
@@ -1,24 +1,23 @@
+/*
+Problem Statemtent: https://cses.fi/problemset/task/1137/
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
 
-#define vt vector
-#define sz(x) (int)(x).size()
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
+#define ar array
 
 const int mxN=2e5;
-int n, q, ds[mxN], de[mxN], dt;
-ll arr[mxN], ft[mxN+1];
-vt<int> adj[mxN];
+int n, q, a[mxN], dt, ds[mxN], de[mxN];
+vector<int> adj[mxN];
+ll ft[mxN+1];
 
 void dfs(int u=0, int p=-1) {
 	ds[u]=dt++;
-	for(int v: adj[u]) {
-		if(v==p)
-			continue;
-		dfs(v, u);
-	}
+	for(int v: adj[u])
+		if(v^p)
+			dfs(v, u);
 	de[u]=dt;
 }
 
@@ -29,42 +28,47 @@ void upd(int i, int x) {
 
 ll qry(int i) {
 	ll r=0;
-	for(; i; i-=i&-i)
+	for(; i>0; i-=i&-i)
 		r+=ft[i];
 	return r;
 }
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-
+void solve() {
 	cin >> n >> q;
 	for(int i=0; i<n; ++i)
-		cin >> arr[i];
-	for(int i=0, a, b; i<n-1; ++i) {
+		cin >> a[i];
+	for(int i=1, a, b; i<n; ++i) {
 		cin >> a >> b, --a, --b;
-		adj[a].pb(b);
-		adj[b].pb(a);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
 
 	dfs();
 	for(int i=0; i<n; ++i)
-		upd(ds[i], arr[i]);
+		upd(ds[i], a[i]);
 
 	while(q--) {
-		int qt;
+		int qt, s, x;
 		cin >> qt;
 		if(qt==1) {
-			int s, x;
 			cin >> s >> x, --s;
-			upd(ds[s], x-arr[s]);
-			arr[s]=x;
+			upd(ds[s], x-a[s]);
+			a[s]=x;
 		} else {
 			int s;
 			cin >> s, --s;
-			cout << qry(de[s])-qry(ds[s]) << '\n';
+			cout << (qry(de[s])-qry(ds[s])) << "\n";
 		}
 	}
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
