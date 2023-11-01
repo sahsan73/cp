694dfb15095e57369ca1eb2b90d5bf302a10a1b3
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-11-01 09:33:00 +0530

    Update Sum of Two Values.cpp

--- Problems-&&-Solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
+++ Problems-&&-Solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -3,6 +3,11 @@ Problem Statement: https://cses.fi/problemset/task/1640
 */
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Using unordered_map with some hacks
+
+TC = O(n)
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
@@ -10,29 +15,35 @@ using ll=long long;
 #define ar array
 
 const int mxN=2e5;
-int n;
-ll x;
-ar<ll, 2> a[mxN];
+int n, x, a[mxN];
 
-int main() {
+void solve() {
 	cin >> n >> x;
 	for(int i=0; i<n; ++i)
-		cin >> a[i][0], a[i][1]=i+1;
+		cin >> a[i];
 
-	/*
-	 * If you try to use unordered_map, you will get TLE. As because, in worst case, count
-  	 * operation works in O(n) time.
-	*/
-	map<ll, ll> mp;
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	int y=uniform_int_distribution<int>(69, 69696969)(rng);
+
+	unordered_map<int, int> mp;
 	for(int i=0; i<n; ++i) {
-		if(mp.count(x-a[i][0])) {
-			cout << mp[x-a[i][0]] << " " << a[i][1];
-			return 0;
+		if(mp.count((x-a[i])^y)) {
+			cout << mp[(x-a[i])^y]+1 << " " << i+1;
+			return;
 		}
-		mp[a[i][0]]=a[i][1];
+		mp[a[i]^y]=i;
 	}
 	cout << "IMPOSSIBLE";
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
 
@@ -44,30 +55,75 @@ using ll=long long;
 #define ar array
 
 const int mxN=2e5;
-int n;
-ll x;
-ar<ll, 2> a[mxN];
+int n, x, a[mxN];
+
+void solve() {
+	cin >> n >> x;
+	for(int i=0; i<n; ++i)
+		cin >> a[i];
+	/*
+	 * If you try to use unordered_map, you may get TLE. As because, in worst case, count
+  	 * operation works in O(n) time.
+	*/
+	map<int, int> mp;
+	for(int i=0; i<n; ++i) {
+		if(mp.count(x-a[i])) {
+			cout << mp[x-a[i]]+1 << " " << i+1;
+			return;
+		}
+		mp[a[i]]=i;
+	}
+	cout << "IMPOSSIBLE";
+}
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+#define ar array
+
+const int mxN=2e5;
+int n, x;
+ar<int, 2> a[mxN];
+
+void solve() {
 	cin >> n >> x;
 	for(int i=0; i<n; ++i)
 		cin >> a[i][0], a[i][1]=i+1;
-
 	sort(a, a+n);
-	int i=0, j=n-1;
-	while(i<j) {
-		ll s=a[i][0]+a[j][0];
+	for(int i=0, j=n-1; i<j;) {
+		int s=a[i][0]+a[j][0];
 		if(s==x) {
 			cout << a[i][1] << " " << a[j][1];
-			return 0;
+			return;
 		}
-
 		if(s<x)
 			++i;
 		else
 			--j;
 	}
 	cout << "IMPOSSIBLE";
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
