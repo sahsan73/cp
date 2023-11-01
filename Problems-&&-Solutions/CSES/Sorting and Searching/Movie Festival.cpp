5c6b76b7ef0eb936b3403bab8198f2f22274610a
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-11-01 10:00:04 +0530

    Update Movie Festival.cpp

--- Problems-&&-Solutions/CSES/Sorting and Searching/Movie Festival.cpp
+++ Problems-&&-Solutions/CSES/Sorting and Searching/Movie Festival.cpp
@@ -15,24 +15,31 @@ const int mxN=2e5;
 int n;
 ar<int, 2> t[mxN];
 
-int main() {
+void solve() {
 	cin >> n;
-	for(int i=0; i<n; ++i)
+	for(int i=0; i<n; ++i) {
 		cin >> t[i][0] >> t[i][1];
-
-	sort(t, t+n, [](auto &p, auto &q){
-		return p[1]<q[1];
+	}
+	sort(t, t+n, [](ar<int, 2> x, ar<int, 2> y) {
+		return x[1]<y[1];
 	});
 	int cnt=1;
-	int a=t[0][0], b=t[0][1];
-	for(int i=1; i<n; ++i) {
-		int x=t[i][0], y=t[i][1];
-		if(x<b)
-			continue;
-		++cnt;
-		a=x, b=y;
+	for(int i=1, b=t[0][1]; i<n; ++i) {
+		if(b<=t[i][0]) {
+			b=t[i][1];
+			++cnt;
+		}
 	}
 	cout << cnt;
+}
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
 	return 0;
 }
