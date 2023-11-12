f5f1e77df1d5206f31f242547c8ba540a93da255
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-11-12 16:45:41 +0530

    Update Rectangle Cutting.cpp

--- Problems-&&-Solutions/CSES/Dynamic Programming/Rectangle Cutting.cpp
+++ Problems-&&-Solutions/CSES/Dynamic Programming/Rectangle Cutting.cpp
@@ -11,34 +11,29 @@ using ll=long long;
 const int mxN=5e2;
 int a, b, dp[mxN+1][mxN+1];
 
-void solve() {
-	cin >> a >> b;
-	for(int i=0; i<=a; ++i) {
-		for(int j=0; j<=b; ++j) {
-			if(i==j) {
-				// they're already squares
-				dp[i][j]=0;
-			} else {
-				dp[i][j]=1e9;
-				// horizontal cuts
-				for(int k=1; k<i; ++k)
-					dp[i][j]=min(dp[i][j], dp[k][j]+dp[i-k][j]+1);
-				// vertical cuts
-				for(int k=1; k<j; ++k)
-					dp[i][j]=min(dp[i][j], dp[i][k]+dp[i][j-k]+1);
-			}
-		}
-	}
-	cout << dp[a][b];
-}
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int t=1;
-	//cin >> t;
-	while(t--)
-		solve();
+	cin >> a >> b;
+	// dp[i][j]=minimum cuts required for ixj sheet
+	for(int i=0; i<=a; ++i) {
+		for(int j=0; j<=b; ++j) {
+			if(i==j) // they're already squares
+				dp[i][j]=0;
+			else {
+				dp[i][j]=1e9;
+				// horizontal cuts
+				for(int k=1; k<i; ++k)
+					dp[i][j]=min(dp[i][j], 1+dp[k][j]+dp[i-k][j]);
+
+				// vertical cuts
+				for(int k=1; k<j; ++k)
+					dp[i][j]=min(dp[i][j], 1+dp[i][k]+dp[i][j-k]);
+			}
+		}
+	}
+	cout << dp[a][b];
+
 	return 0;
 }
