b155840a8e24bbe11f483b85bfd584bdbca039d4
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-11-26 00:31:53 +0530

    Update Shortest Routes I.cpp

--- Problems-&&-Solutions/CSES/Graph Algorithms/Shortest Routes I.cpp
+++ Problems-&&-Solutions/CSES/Graph Algorithms/Shortest Routes I.cpp
@@ -1,3 +1,7 @@
+/*
+Problem Statement: https://cses.fi/problemset/task/1671
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
@@ -6,38 +10,32 @@ using ll=long long;
 
 const int mxN=1e5;
 int n, m;
+vector<ar<int, 2>> adj[mxN];
 ll d[mxN];
-vector<ar<ll, 2>> adj[mxN];
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	cin >> n >> m;
-	for(int i=0,; i<m; ++i) {
-		ll a, b, c;
+	for(int i=0, a, b, c; i<m; ++i) {
 		cin >> a >> b >> c, --a, --b;
 		adj[a].push_back({c, b});
 	}
 
-	memset(d, 0x3f, sizeof(d));
-	d[0]=0;
 	priority_queue<ar<ll, 2>, vector<ar<ll, 2>>, greater<ar<ll, 2>>> pq;
 	pq.push({0, 0});
+	memset(d, 0x3f, sizeof(d));
+	d[0]=0;
 	while(!pq.empty()) {
 		ar<ll, 2> u=pq.top();
 		pq.pop();
 		if(u[0]>d[u[1]])
 			continue;
-
-		for(ar<ll, 2> v: adj[u[1]]) {
-			if(u[0]+v[0]<d[v[1]]) {
-				d[v[1]]=u[0]+v[0];
+		for(ar<int, 2> v: adj[u[1]]) {
+			if(v[0]+u[0]<d[v[1]]) {
+				d[v[1]]=v[0]+u[0];
 				pq.push({d[v[1]], v[1]});
 			}
 		}
 	}
-
 	for(int i=0; i<n; ++i)
 		cout << d[i] << " ";
 	
