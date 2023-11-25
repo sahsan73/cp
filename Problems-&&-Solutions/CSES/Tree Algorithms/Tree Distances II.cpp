823fd83511ef3bc5b46f269a82d69be23d08a114
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-11-25 13:25:20 +0530

    Update Tree Distances II.cpp

--- Problems-&&-Solutions/CSES/Tree Algorithms/Tree Distances II.cpp
+++ Problems-&&-Solutions/CSES/Tree Algorithms/Tree Distances II.cpp
@@ -1,11 +1,17 @@
+/*
+Problem Statement: https://cses.fi/problemset/task/1133/
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
 
+#define ar array
+
 const int mxN=2e5;
-int n, d[mxN];
-ll s[mxN], ans[mxN];
+int n, d[mxN], s[mxN];
 vector<int> adj[mxN];
+ll ans[mxN];
 
 void dfs(int u=0, int p=-1) {
 	s[u]=1;
@@ -18,31 +24,27 @@ void dfs(int u=0, int p=-1) {
 	}
 }
 
-void dfs2(int u, ll sd, int p=-1) {
+void dfs2(int u, int p, ll sd) {
+	//cout << "(" << u << ", " << p << ", " << s << ")\n";
 	ans[u]=sd;
 	for(int v: adj[u]) {
-		if(v==p)
-			continue;
-		dfs2(v, sd-s[v]+(n-s[v]), u);
+		if(v^p)
+			dfs2(v, u, sd+(n-s[v])-s[v]);
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	cin >> n;
 	for(int i=1, a, b; i<n; ++i) {
 		cin >> a >> b, --a, --b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
-
 	dfs();
 	ll sd=0;
 	for(int i=0; i<n; ++i)
 		sd+=d[i];
-	dfs2(0, sd);
+	dfs2(0, -1, sd);
 	for(int i=0; i<n; ++i)
 		cout << ans[i] << " ";
 
