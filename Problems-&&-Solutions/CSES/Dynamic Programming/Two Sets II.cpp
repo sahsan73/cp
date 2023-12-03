c8ad180ec2ddcf8e945067da90bd913e284a0f9c
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-12-03 16:36:33 +0530

    Update Two Sets II.cpp

--- Problems-&&-Solutions/CSES/Dynamic Programming/Two Sets II.cpp
+++ Problems-&&-Solutions/CSES/Dynamic Programming/Two Sets II.cpp
@@ -6,40 +6,24 @@ Problem Statement: https://cses.fi/problemset/task/1093
 using namespace std;
 using ll=long long;
 
-#define ar array
-
-const int mxN=500, mxS=1.3e5, M=1e9+7;
-int n;
-ll dp[mxN+1][mxS];
-
-void solve() {
-	cin >> n;
-	int s=n*(n+1)/2;
-	if(s&1) {
-		cout << 0;
-		return;
-	}
-	s/=2;
-	for(int i=0; i<=n; ++i)
-		dp[i][0]=1;
-	for(int i=1; i<=n; ++i) {
-		for(int j=1; j<=s; ++j) {
-			dp[i][j]=dp[i-1][j];
-			if(j>=i)
-				dp[i][j]+=dp[i-1][j-i];
-			dp[i][j]%=M;
-		}
-	}
-	cout << dp[n][s]*((M+1)/2)%M; // IMPORTANT
-}
+const int mxN=5e2, mxX=mxN*(mxN+1)/2, M=1e9+7;
+int n, x;
+ll dp[mxX+1];
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin >> n;
+	x=n*(n+1)/2;
+	if(x&1) {
+		cout << 0;
+		return 0;
+	}
 
-	int t=1;
-	//cin >> t;
-	while(t--)
-		solve();
+	dp[0]=1;
+	x/=2; // target sum
+	for(int i=1; i<=n; ++i)
+		for(int j=x; j>=i; --j)
+			dp[j]=(dp[j]+dp[j-i])%M;
+	cout << dp[x]*((M+1)/2)%M; // IMPORTANT
+
 	return 0;
 }
