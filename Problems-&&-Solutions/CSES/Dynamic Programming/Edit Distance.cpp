3b3e1f94d536b840d1fe3962c48c8911e4e2ba24
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-12-03 11:43:28 +0530

    Update Edit Distance.cpp

--- Problems-&&-Solutions/CSES/Dynamic Programming/Edit Distance.cpp
+++ Problems-&&-Solutions/CSES/Dynamic Programming/Edit Distance.cpp
@@ -2,91 +2,114 @@
 Problem STatement: https://cses.fi/problemset/task/1639
 */
 
-
-// IMPLEMENTATION: BOTTOM-UP
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+IMPLEMENTATION: BOTTOM-UP
+	- C style string implementation
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
 
-#define ar array
-
 const int mxN=5e3;
-int dp[mxN+1][mxN+1];
-string s, t;
+int n, m, dp[mxN+1][mxN+1];
+char s[mxN+2], t[mxN+2];
 
-void solve() {
-	cin >> s >> t;
-	int n=s.size(), m=t.size();
+int main() {
+	cin >> s+1 >> t+1;
+
+	n=strlen(s+1), m=strlen(t+1);
 	memset(dp, 0x3f, sizeof(dp));
-	dp[0][0]=0;
-	for(int i=1; i<=n; ++i) {
+	for(int i=0; i<=n; ++i)
 		dp[i][0]=i;
+	for(int j=1; j<=m; ++j)
+		dp[0][j]=j;
+
+	for(int i=1; i<=n; ++i) {
 		for(int j=1; j<=m; ++j) {
-			dp[0][j]=j;
-			if(s[i-1]==t[j-1])
+			if(s[i]==t[j])
 				dp[i][j]=dp[i-1][j-1];
 			else {
-				int x=dp[i][j-1]; // add
-				int y=dp[i-1][j]; // remove
-				int z=dp[i-1][j-1]; // replace
-				dp[i][j]=1+min({x, y, z});
+				//                 add        remove      replace
+				dp[i][j]=1+min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
 			}
 		}
 	}
 	cout << dp[n][m];
-}
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int t=1;
-	//cin >> t;
-	while(t--)
-		solve();
 	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// IMPLEMENTATION: TOP-DOWN
+/*
+IMPLEMENTATION: BOTTOM-UP
+	- C++ style string implementation
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
 
-#define ar array
-
 const int mxN=5e3;
-int dp[mxN+1][mxN+1];
+int n, m, dp[mxN+1][mxN+1];
 string s, t;
 
-int dfs(int n, int m) {
-	if(n==0)
-		return m;
-	if(m==0)
-		return n;
-	if(~dp[n][m])
-		return dp[n][m];
-	if(s[n-1]==t[m-1])
-		return dp[n][m]=dfs(n-1, m-1);
-	int x=dfs(n, m-1); // add
-	int y=dfs(n-1, m); // remove
-	int z=dfs(n-1, m-1); // replace
-	return dp[n][m]=1+min({x, y, z});
+int main() {
+	cin >> s >> t;
+
+	n=s.size(), m=t.size();
+	memset(dp, 0x3f, sizeof(dp));
+	for(int i=0; i<=n; ++i)
+		dp[i][0]=i;
+	for(int j=1; j<=m; ++j)
+		dp[0][j]=j;
+
+	for(int i=1; i<=n; ++i) {
+		for(int j=1; j<=m; ++j) {
+			if(s[i-1]==t[j-1])
+				dp[i][j]=dp[i-1][j-1];
+			else {
+				//                 add        remove      replace
+				dp[i][j]=1+min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+			}
+		}
+	}
+	cout << dp[n][m];
+
+	return 0;
 }
 
-void solve() {
-	cin >> s >> t;
-	memset(dp, -1, sizeof(dp));
-	cout << dfs(s.size(), t.size());
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+IMPLEMENTATION: TOP-DOWN
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+const int mxN=5e3;
+int n, m, dp[mxN+1][mxN+1];
+string s, t;
+
+int dfs(int i=n, int j=m) {
+	if(i==0)
+		return j;
+	if(j==0)
+		return i;
+	if(~dp[i][j])
+		return dp[i][j];
+	
+	if(s[i-1]==t[j-1])
+		return dp[i][j]=dfs(i-1, j-1);
+	//                         add        remove       replace
+	return dp[i][j]=1+min({dfs(i-1, j), dfs(i, j-1), dfs(i-1, j-1)});
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin >> s >> t;
 
-	int t=1;
-	//cin >> t;
-	while(t--)
-		solve();
+	n=s.size(), m=t.size();
+	memset(dp, -1, sizeof(dp));
+	cout << dfs();
+
 	return 0;
 }
