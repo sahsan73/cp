a1ddc5c87c75d6ad856a4d493e36a35d8857b5db
Saquib Ahsan <85388160+sahsan73@users.noreply.github.com>
2023-12-03 17:17:20 +0530

    Update Coin Combinations I.cpp

--- Problems-&&-Solutions/CSES/Dynamic Programming/Coin Combinations I.cpp
+++ Problems-&&-Solutions/CSES/Dynamic Programming/Coin Combinations I.cpp
@@ -6,68 +6,26 @@ APPROACH: O/N(UNBOUNDED KNAPSACK)
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-Implementation: Bottom-Up(Iteration + Tabulation)
+Implementation: Bottom-Up
 */
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
-const int mxN=100, mxX=1e6, M=1e9+7;
-int n, x, c[mxN], dp[mxX+1];
+const int mxN=1e2, mxX=1e6, M=1e9+7;
+int n, x, c[mxN+1], dp[mxX+1];
 
 int main() {
 	cin >> n >> x;
-	for(int i=0; i<n; ++i)
+	for(int i=1; i<=n; ++i)
 		cin >> c[i];
-	
+
 	dp[0]=1;
-	for(int j=1; j<=x; ++j) {
-		int res=0;
-		for(int i=0; i<n; ++i) {
-			if(j>=c[i]) {
-				res+=dp[j-c[i]];
-				res%=M;
-			}
-		}
-		dp[j]=res;
-	}
+	for(int j=1; j<=x; ++j)
+		for(int i=1; i<=n; ++i)
+			if(j>=c[i])
+				dp[j]=(dp[j]+dp[j-c[i]])%M;
 	cout << dp[x];
-
-	return 0;
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-Implementation: Top-Down(Recursion + Memoization)
-*/
-#include <bits/stdc++.h>
-using namespace std;
-
-const int mxN=100, mxX=1e6, M=1e9+7;
-int n, x, c[mxN], dp[mxX+1];
-
-int dfs(int x) {
-	if(x==0)
-		return 1;
-	if(~dp[x])
-		return dp[x];
-
-	int res=0;
-	for(int i=0; i<n; ++i) {
-		if(x>=c[i]) {
-			res+=dfs(x-c[i]);
-			res%=M;
-		}
-	}
-	return dp[x]=res;
-}
-
-int main() {
-	cin >> n >> x;
-	for(int i=0; i<n; ++i)
-		cin >> c[i];
-
-	memset(dp, -1, sizeof(dp));
-	cout << dfs(x);
 
 	return 0;
 }
